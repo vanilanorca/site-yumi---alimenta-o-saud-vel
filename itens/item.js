75d013c95ae7f456f721a59bd64ab88cb9c6c314
Vinicius Lopes Alves <->
2021-10-06 16:08:59 -0300

    .

--- itens/item.js
+++ itens/item.js
@@ -8,8 +8,23 @@ window.onload = (function () {
     window.setInterval('load()',1500);})
     
 
+// ======== HAMBURGER MENU =========
+var hambMenu = document.getElementById("hamb-menu");
+var closeMenu = document.getElementById("close-menu");
+var nav = document.getElementById("nav");
+
+function openMenu(){
+    nav.style.display = "flex"
+    closeMenu.style.display = "inherit"
+}
+
+function closeMenuFunction(){
+    nav.style.display = "none"
+    hambMenu.style.display = "inherit"
+}
 
 
+// ======== JSON =========
 var myInit = {
     method: 'GET',
     headers: {
